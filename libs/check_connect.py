5eb3e3c68c9540672a373107ef0838364554c658
nikitinas <as.nikitin@promenergo-rt.ru>
2025-10-20 16:19:02 +0300

    ОБНОВЛЕНИЕ: - добавил проверку ТТ в проверку соединения ведомых счетчиков - поправил в этой же функции вывод в логи

--- libs/check_connect.py
+++ libs/check_connect.py
@@ -40,16 +40,18 @@ def check_connect_for_wingman_meter(connect, com, text):
             settings.media.open()
         reader.initializeConnection()
 
-        relay = GXDLMSDisconnectControl("0.0.96.3.10.255")
-        if reader.read(relay, 4) != 2:
-            relay.controlMode = 2
-            reader.write(relay, 4)
-            print('Установлен режим реле 2')
-        reader.relay_disconnect()
-        reader.relay_reconnect()
+        if reader.deviceType != 'TT':
+            relay = GXDLMSDisconnectControl("0.0.96.3.10.255")
+            if reader.read(relay, 4) != 2:
+                relay.controlMode = 2
+                reader.write(relay, 4)
+                print('Установлен режим реле 2')
+            reader.relay_disconnect()
+            reader.relay_reconnect()
 
         reader.close()
         print(f'{text} >> Соединение корректное')
     except Exception as e:
         settings.media.close()
-        raise Exception(f'{text} >> Не удается установить соединение с ошибкой {e}. Остановка программы.')
+        raise Exception(f'{text} >> Не удается установить соединение при проверке соединения с {text[-16:]}'
+                        f' с ошибкой {e}. Остановка программы.')
